55b3c25a4af52bf9d8426328572419fbe6a49039
kaynan <78617015+kaynanxd@users.noreply.github.com>
2025-11-24 22:21:45 -0300

    Add files via upload

---
+++ centralexpert/centralexpertTests/centralexpertTests.swift
@@ -0,0 +1,17 @@
+//
+//  centralexpertTests.swift
+//  centralexpertTests
+//
+//  Created by Iredefbmac_08 on 24/11/25.
+//
+
+import Testing
+@testable import centralexpert
+
+struct centralexpertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
