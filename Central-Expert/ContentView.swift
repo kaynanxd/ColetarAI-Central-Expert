4a4ce42ffcc6e5b6f60ccaaa380ab241f379aa2c
Fellipe Almeida <150681795+FellipeAlmeida@users.noreply.github.com>
2025-12-05 04:13:25 -0300

    feat: add icons

--- Central-Expert/ContentView.swift
+++ Central-Expert/ContentView.swift
@@ -11,61 +11,63 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HStack {
-                Text("Icone1")
+                Image("logo 16")
+                    .frame(width: 20)
+                Text("Coletar**AI**")
                 Spacer()
-                Text("Icone2")
+                Image("jijoca")
             }
             HStack() {
-                Text("Icone3")
+                Image("Arrow 1")
                 Spacer()
             }
             HStack{
-                Text("Coletar")
-                
+                Text("**Coletar**")
+                    .font(.system(size: 24))
                 Spacer()
-                Text("Status: Disponivel")
+                Text("**Status**:")
+                Text("Disponível")
+                    .foregroundStyle(.green)
             }
             HStack {
                 Image("Image")
                     .resizable()
-                    .frame(width: 400, height: 250)
+                    .frame(width: 359, height: 188)
             }
+        
             VStack(alignment: .leading) {
-                HStack() {
-                    Text("Teste teste")
-                        .frame(width: 130, height: 30)
-                        .background(Color.green)
-                    Spacer()
-                    Text("Teste teste")
-                        .frame(width: 130, height: 30)
-                        .background(Color.green)
-                }
-                VStack(){
-                    HStack() {
-                        Text("280kg")
+                HStack(){
+                    VStack() {
+                        Text("LIXO COLETADO")
+                            .foregroundStyle(Color.white)
+                            .frame(width: 130, height: 30)
+                        HStack{
+                            Text("280")
+                                .font(.system(size: 40))
+                                .foregroundStyle(Color.white)
+                            Text("kg")
+                                .foregroundStyle(Color.white)
+                        }
+                    }
+                    .frame(width: 169, height: 79)
+                    .background(Color.green)
+                    
+                    VStack {
+                        Text("META DIÁRIA")
+                            .frame(width: 130, height: 30)
+                                                
+                        Text("50kg")
                             .font(.system(size: 25))
                             .frame(width: 100, height: 30)
-                            .background(Color.green)
-
-                        Spacer()
-                        
-                        Text("280kg")
-                            .font(.system(size: 25))
-                            .frame(width: 100, height: 30)
-                            .background(Color.green)
-                                        }
-                    Spacer()
-                        .frame(height: 30)
-                        HStack(alignment: .center) {
-                            
-                            Text("Icone4")
-                                .background(Color.green)
-                            
-                            Text("RELATAR PROBLEMA")
-                                .frame(maxWidth: .infinity, alignment: .center)
-                                .background(Color.green)
                     }
                 }
+            }
+                    
+            HStack() {
+                Text("Icone4")
+                
+                Text("RELATAR PROBLEMA")
+                    .frame(maxWidth: .infinity, alignment: .center)
             }
         }
         .padding()
